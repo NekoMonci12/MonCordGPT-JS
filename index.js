1af05b64458b09fc3135be83123d830eceee4672
Muhammad Tamir <muhammad218477@smk.belajar.id>
2024-01-29 12:10:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const gtts = require('gtts');
+const { gtts } = require('node-gtts');
 const openai = require('openai');
 const ytdl = require('ytdl-core');
 
@@ -27,13 +27,44 @@ bot.on('messageCreate', async (message) => {
         const [command, ...args] = message.content.slice(prefix.length).split(' ');
 
         if (command === 'tts') {
-            // TTS functionality
+            const text = args.join(' ');
+            const speech = new gtts(text, 'en');
+            speech.save('tts.mp3', function () {
+                const voiceChannel = message.member.voice.channel;
+                if (voiceChannel) {
+                    voiceChannel.join().then(connection => {
+                        const dispatcher = connection.play('./tts.mp3');
+                        dispatcher.on('finish', () => voiceChannel.leave());
+                    });
+                } else {
+                    message.reply('You need to be in a voice channel to use this command.');
+                }
+            });
         } else if (command === 'openai') {
-            // OpenAI functionality
+            const prompt = args.join(' ');
+            const response = await openai.Completion.create({
+                engine: 'davinci',
+                prompt,
+                max_tokens: 150,
+            });
+            message.channel.send(response.choices[0].text);
         } else if (command === 'avatar') {
-            // Avatar functionality
+            const user = message.mentions.users.first() || message.author;
+            message.channel.send(user.displayAvatarURL({ dynamic: true }));
         } else if (command === 'download') {
-            // Media downloading functionality
+            const url = args[0];
+            if (!url) {
+                return message.reply('Please provide a valid YouTube URL.');
+            }
+            const voiceChannel = message.member.voice.channel;
+            if (voiceChannel) {
+                const connection = await voiceChannel.join();
+                const stream = ytdl(url, { filter: 'audioonly' });
+                const dispatcher = connection.play(stream);
+                dispatcher.on('finish', () => voiceChannel.leave());
+            } else {
+                message.reply('You need to be in a voice channel to use this command.');
+            }
         }
     }
 });
